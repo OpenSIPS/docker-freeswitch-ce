224393c8b03fab56128df9a454ac76c641460513
Răzvan Crainea <razvan@opensips.org>
2024-07-12 10:45:18 +0300

    Add licence for xml_handler.lua

--- xml_handler.lua
+++ xml_handler.lua
@@ -1,4 +1,22 @@
+-- Docker Freeswitch for OpenSIPS Community Edtion
+-- Copyright (C) 2024 OpenSIPS Solutions
 
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-- Inspired from Giovanni Maruzzelli (gmaruzz@gmail.com) tutorial:
+-- https://www.opensips.org/Documentation/Tutorials-OpenSIPSFreeSwitchIntegration#toc19
+    
 freeswitch.consoleLog("notice", "[xml_handler] Script loaded successfully.\n");
 
 if (XML_REQUEST["section"] == "directory") then
